ecb7f701009eb54ec60df18b0cdc7f05ee5dab32
Tengis <orusov22112001@mail.ru>
2020-02-27 13:02:49 -0300

    third_add run.sh

--- task1/run.sh
+++ task1/run.sh
@@ -2,9 +2,11 @@
 mkdir $3
 a=$(find $1 -depth -name "*.$2")
 n=0
+sep="_"
 for file in $a
 do
-cp $file $3/"$n.$2"
+fname=$(basename -s .$2 $file)
+cp $file $3/"$fname$sep$n.$2"
 n=$(($n+1))
 done
 
